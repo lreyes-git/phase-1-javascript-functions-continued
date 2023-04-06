0688ddebf7a8fd279bed08e694d70be69ea490ed
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-04-06 16:43:35 -0700

    JavaScript functions cont lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(someString) {
+    if (someString === undefined) {
+        someString = "roller-skate";
+        return `This Saturday, I want to ${someString}!`;
+    } else {
+        return `This Saturday, I want to ${someString}!`;
+    }
+}
+
+const mondayWork = function(someOtherString) {
+    if (someOtherString === undefined) {
+        someOtherString = "go to the office";
+        return `This Monday, I will ${someOtherString}.`;
+    } else {
+        return `This Monday, I will ${someOtherString}.`;
+    }
+}
+
+function wrapAdjective (flair = "*") {
+    const encouragingPromptFunction = function (adj = "special") {
+        return `You are ${flair}${adj}${flair}!`;
+    };
+    return encouragingPromptFunction;
+}
+
+
